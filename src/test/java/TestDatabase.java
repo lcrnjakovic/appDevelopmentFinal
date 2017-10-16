7b4659e05edff00be4789dc610a2382b2f2a0cea
lcrnjakovic <lxc7826@rit.edu>
2017-10-15 21:32:15 -0400

    Created TestDatabase for testing connection to a database

--- src/test/java/TestDatabase.java
+++ src/test/java/TestDatabase.java
@@ -1,6 +1,27 @@
+import org.junit.Test;
+
+import java.sql.*;
+
+import static org.junit.Assert.assertEquals;
+
 /**
  * Created by lukacrnjakovic on 10/15/17.
  */
 public class TestDatabase {
+
+    @Test
+    public void testScriptSyntax(){
+        String connSuccess = "";
+        try{
+            DriverManager.getConnection("jdbc:mysql://localhost:3306/mydb?verifyServerCertificate=false&useSSL=true","root","student");
+            connSuccess = "connected";
+        }
+        catch (SQLException sql ) {
+            connSuccess = "error";
+            sql.printStackTrace();
+        }
+        assertEquals("connected", connSuccess);
+
+    }
 
 }
