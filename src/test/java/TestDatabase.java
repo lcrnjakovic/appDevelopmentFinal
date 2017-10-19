e667c07453be3a6def55a4d31455957dc504e243
lcrnjakovic <lxc7826@rit.edu>
2017-10-18 21:42:09 -0400

    Completed test methods which test mysqlcheck output

--- src/test/java/TestDatabase.java
+++ src/test/java/TestDatabase.java
@@ -4,8 +4,6 @@ import java.io.*;
 import java.sql.*;
 import java.util.Scanner;
 
-import static org.hamcrest.CoreMatchers.not;
-import static org.hamcrest.CoreMatchers.is;
 import static org.junit.Assert.*;
 
 /**
@@ -13,6 +11,9 @@ import static org.junit.Assert.*;
  */
 public class TestDatabase {
 
+    /**
+     * Testing connection to the database
+     */
     @Test
     public void testDBConnection(){
         String connSuccess = "";
@@ -28,51 +29,59 @@ public class TestDatabase {
 
     }
 
-    @Test
-    public void testSelectSyntax(){
-        Connection conn = null;
-        ResultSet rs = null;
-        try{
-            File script = new File("table.sql");
-            conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/mydb?verifyServerCertificate=false&useSSL=true","root","student");
-            assertTrue(script.exists());
-            String fileAsString = new Scanner(script).useDelimiter("\\Z").next();
-            System.out.println(fileAsString);
-            Statement statement = conn.createStatement();
-            rs = statement.executeQuery(fileAsString);
-        }
-        catch (SQLException sql ) {
-            assertNotNull(conn);
-            assertNotNull(rs);
-            sql.printStackTrace();
-        } catch (FileNotFoundException e) {
-            e.printStackTrace();
-        }
-    }
-
+    /**
+     * Testing mysqlcheck output
+     * Currently testing valid sql file
+     * In order to test empty or invalid files, swap filename and switch boolean values in asserts at the bottom
+     */
     @Test
     public void testCreateStatementSyntax(){
-        Connection conn = null;
+        String outputForTesting;
+        Connection conn;
         try{
             File script = new File("table.sql");
             conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/mydb?verifyServerCertificate=false&useSSL=true","root","student");
-            assertTrue(script.exists());
-            String fileAsString = new Scanner(script).useDelimiter("\\Z").next();
-            System.out.println(fileAsString);
-            Statement statement = conn.createStatement();
-            Scanner scanner = new Scanner(fileAsString);
-            while (scanner.hasNextLine()) {
-                String line = scanner.nextLine();
-                statement.executeUpdate(line);
+            if(!script.exists()){
+                outputForTesting = "error: File not found.";
             }
-            scanner.close();
+            else if(script.length() < 1){
+                outputForTesting = "error: Empty file.";
+            }
+            else{
+                String fileAsString = new Scanner(script).useDelimiter("\\Z").next();
+                Statement statement = conn.createStatement();
+                Scanner scanner = new Scanner(fileAsString);
+                while (scanner.hasNextLine()) {
+                    String line = scanner.nextLine();
+                    statement.executeUpdate(line);
+                }
+                scanner.close();
+                StringBuffer output = new StringBuffer();
+                Process p;
+                try {
+                    p = Runtime.getRuntime().exec("/usr/local/mysql/bin/mysqlcheck -u root -pstudent -c mydb");
+                    p.waitFor();
+                    BufferedReader reader =
+                            new BufferedReader(new InputStreamReader(p.getInputStream()));
+                    String line = "";
+                    while ((line = reader.readLine())!= null) {
+                        output.append(line + "\n");
+                    }
+
+                } catch (Exception e) {
+                    outputForTesting = e.toString();
+                }
+                outputForTesting = output.toString();
+            }
         }
-        catch (SQLException sql ) {
-            assertNotNull(conn);
-            sql.printStackTrace();
-        } catch (FileNotFoundException e) {
-            e.printStackTrace();
+        catch (SQLException | FileNotFoundException sql ) {
+            outputForTesting = sql.toString();
         }
+
+        // switching the booleans tests for errors and vice versa
+        assertEquals(outputForTesting.contains("error"), false);
+        assertEquals(outputForTesting.contains("student") || outputForTesting.contains("courses") || outputForTesting.contains("faculty"), true);
+
     }
 
 }
